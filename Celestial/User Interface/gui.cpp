fb939f08910c089af3b3ffaaa3604086b30ff9e9
estetik <128476710+estkz@users.noreply.github.com>
2023-03-24 23:55:11 +0100

    Add files via upload

--- Celestial/User Interface/gui.cpp
+++ Celestial/User Interface/gui.cpp
@@ -1,34 +1,84 @@
 #include "gui.h"
 
-void CreateHWindow(const char* windowName, const char* className) {
+#include "../imgui/imgui_impl_dx9.h"
+#include "../imgui/imgui_impl_win32.h"
+#include "../imgui/imgui.h"
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND window, UINT message, WPARAM wideParamater, LPARAM longParameter);
+
+long __stdcall WindowProcess(HWND window, UINT message, WPARAM wideParameter, LPARAM longParameter) {
+	if (ImGui_ImplWin32_WndProcHandler(window, message, wideParameter, longParameter)) {
+		return true;
+	}
+
+	switch (message) {
+
+	case WM_SIZE: {
+		if (gui::device && wideParameter != SIZE_MINIMIZED) {
+			gui::presentParameters.BackBufferWidth = LOWORD(longParameter);
+			gui::presentParameters.BackBufferHeight = LOWORD(longParameter);
+			gui::ResetDevice();
+		}
+	}return 0;
+
+
+	case WM_SYSCOMMAND: {
+		if ((wideParameter & 0xfff0) == SC_KEYMENU) { // Disable the ALT application UI
+			return 0;
+		}break;
+
+		
+	case WM_DESTROY: {
+		PostQuitMessage(0);
+	}return 0;
+
+
+	case WM_LBUTTONDOWN: {
+		gui::position = MAKEPOINTS(longParameter); // Set the position of your mouseclick
+	}return 0;
+
+	case WM_MOUSEMOVE: {
+		// Add calculation of mouse movement here:
+		if (wideParameter == MK_LBUTTON) {
+			const auto points = MAKEPOINTS(longParameter);
+			auto rect = ::RECT{ };
+
+			// Expand here
+		}
+	}
+
+	}
+	}
+}
+
+void gui::CreateHWindow(const char* windowName, const char* className) {
 
 }
 
-void DestroyHWindow() {
+void gui::DestroyHWindow() {
 
 }
 
-void ResetDevice() {
+void gui::ResetDevice() {
 
 }
 
-
-void CreateImGui() {
-
-}
-
-void DestroyImGui() {
+void gui::CreateImGui() {
 
 }
 
-void BeginRender() {
+void gui::DestroyImGui() {
 
 }
 
-void EndRender() {
+void gui::BeginRender() {
 
 }
 
-void Render() {
+void gui::EndRender() {
+
+}
+
+void gui::Render() {
 
 }
