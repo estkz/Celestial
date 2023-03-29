2eaf0a9d8105cb1fcf00307c732a9a34eaaac6c1
estkz <3stetik@gmail.com>
2023-03-29 09:33:48 +0200

    init

--- Celestial/User Interface/gui.cpp
+++ Celestial/User Interface/gui.cpp
@@ -6,6 +6,7 @@
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND window, UINT message, WPARAM wideParamater, LPARAM longParameter);
 
+/*
 long __stdcall WindowProcess(HWND window, UINT message, WPARAM wideParameter, LPARAM longParameter) {
 	if (ImGui_ImplWin32_WndProcHandler(window, message, wideParameter, longParameter)) {
 		return true;
@@ -57,35 +58,107 @@ long __stdcall WindowProcess(HWND window, UINT message, WPARAM wideParameter, LP
 	}
 	}
 }
+*/
 
-void gui::CreateHWindow(const char* windowName, const char* className) {
+long __stdcall WindowProcess(
+	HWND window,
+	UINT message,
+	WPARAM wideParameter,
+	LPARAM longParameter)
+{
+	if (ImGui_ImplWin32_WndProcHandler(window, message, wideParameter, longParameter)) {
+		return true;
+	}
+
+	switch (message)
+	{
+	case WM_SIZE: {
+		if (gui::device && wideParameter != SIZE_MINIMIZED)
+		{
+			gui::presentParameters.BackBufferWidth = LOWORD(longParameter);
+			gui::presentParameters.BackBufferHeight = HIWORD(longParameter);
+			gui::ResetDevice();
+		}
+	}return 0;
+
+	case WM_SYSCOMMAND: {
+		if ((wideParameter & 0xfff0) == SC_KEYMENU)
+		{
+			return 0;
+		}
+	}break;
+
+	case WM_DESTROY: {
+		PostQuitMessage(0);
+	}return 0;
+
+	case WM_LBUTTONDOWN: {
+		gui::position = MAKEPOINTS(longParameter); // Set the position of your mouseclick
+	}return 0;
+
+	case WM_MOUSEMOVE: {
+		if (wideParameter == MK_LBUTTON)
+		{
+			const auto points = MAKEPOINTS(longParameter);
+			auto rect = ::RECT{ };
+
+			GetWindowRect(gui::window, &rect);
+
+			rect.left += points.x - gui::position.x;
+			rect.top += points.y - gui::position.y;
+
+			if (gui::position.x >= 0 &&
+				gui::position.x <= gui::WIDTH &&
+				gui::position.y >= 0 && gui::position.y <= 19)
+				SetWindowPos(gui::window, HWND_TOPMOST, rect.left, rect.top, 0, 0, SWP_SHOWWINDOW | SWP_NOSIZE | SWP_NOZORDER
+				);
+		}
+	}return 0;
+
+	}
+
+	return DefWindowProcW(window, message, wideParameter, longParameter);
+}
+
+void gui::CreateHWindow(const char* windowName, const char* className) noexcept {
+	windowClass.cbSize = sizeof(WNDCLASSEXA);
+	windowClass.style = CS_CLASSDC;
+	windowClass.lpfnWndProc = WindowProcess; // Error
+	windowClass.cbClsExtra = 0;
+	windowClass.cbWndExtra = 0;
+	windowClass.hInstance = GetModuleHandleA(0);
+	windowClass.hIcon = 0;
+	windowClass.hCursor = 0;
+	windowClass.hbrBackground = 0;
+	windowClass.lpszMenuName = 0;
+	windowClass.lpszClassName = className;
+	windowClass.hIconSm = 0;
+}
+
+void gui::DestroyHWindow() noexcept {
 
 }
 
-void gui::DestroyHWindow() {
+void gui::ResetDevice() noexcept {
 
 }
 
-void gui::ResetDevice() {
+void gui::CreateImGui() noexcept {
 
 }
 
-void gui::CreateImGui() {
+void gui::DestroyImGui() noexcept {
 
 }
 
-void gui::DestroyImGui() {
+void gui::BeginRender() noexcept {
 
 }
 
-void gui::BeginRender() {
+void gui::EndRender() noexcept {
 
 }
 
-void gui::EndRender() {
-
-}
-
-void gui::Render() {
+void gui::Render() noexcept {
 
 }
