c2ec6288b7cf30bb2dc9eeac275db04ab89cc610
estkz <3stetik@gmail.com>
2023-07-14 15:04:24 +0200

    init

--- Celestial/main.cpp
+++ Celestial/main.cpp
@@ -23,8 +23,22 @@ int __stdcall wWinMain(HINSTANCE instance, HINSTANCE previousInstance, PWSTR arg
 	// Create while isOpened loop
 	// Destroy imgui/Device/HWindow to avoid memory leaks
 
+	gui::CreateHWindow("Celestial");
+	gui::CreateDevice();
+	gui::CreateImGui();
 
+	while (gui::isOpened)
+	{
+		gui::BeginRender();
+		gui::Render();
+		gui::EndRender();
 
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+
+	gui::DestroyImGui();
+	gui::DestroyDevice();
+	gui::DestroyHWindow();
 
 	// Once EXIT_SUCCES gets returned it indicates that the cheat has executed successfully and is now terminating (shutting down).
 	return EXIT_SUCCESS;
--- Celestial/menu/gui.cpp
+++ Celestial/menu/gui.cpp
@@ -88,52 +88,156 @@ long __stdcall WindowProcess(
 		return DefWindowProc(window, message, wideParameter, longParameter);
 }
 
-void CreateHWindow(const char* windowName) noexcept
+void gui::CreateHWindow(const char* windowName) noexcept
 {
+	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.style = CS_CLASSDC;
+	windowClass.lpfnWndProc = WindowProcess;
+	windowClass.cbClsExtra = 0;
+	windowClass.cbWndExtra = 0;
+	windowClass.hInstance = GetModuleHandleA(0);
+	windowClass.hIcon = 0;
+	windowClass.hCursor = 0;
+	windowClass.hbrBackground = 0;
+	windowClass.lpszMenuName = 0;
+	windowClass.lpszClassName = "Celestial";
+	windowClass.hIconSm = 0;
 
+	RegisterClassEx(&windowClass);
+
+	window = CreateWindowExA(0, "Celestial", windowName, WS_POPUP, 100, 100, WIDTH, HEIGHT, 0, 0, windowClass.hInstance, 0);
+
+	ShowWindow(window, SW_SHOWDEFAULT);
+	UpdateWindow(window);
 }
 
-void DestroyHWindow() noexcept
+void gui::DestroyHWindow() noexcept
 {
-
+	DestroyWindow(window);
+	UnregisterClass(windowClass.lpszClassName, windowClass.hInstance);
 }
 
-bool CreateDevice() noexcept
+bool gui::CreateDevice() noexcept
 {
+	d3d = Direct3DCreate9(D3D_SDK_VERSION);
 
+	if (!d3d)
+		return false;
+
+	ZeroMemory(&presentParameters, sizeof(presentParameters));
+
+	presentParameters.Windowed = TRUE;
+	presentParameters.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	presentParameters.BackBufferFormat = D3DFMT_UNKNOWN;
+	presentParameters.EnableAutoDepthStencil = TRUE;
+	presentParameters.AutoDepthStencilFormat = D3DFMT_D16;
+	presentParameters.PresentationInterval = D3DPRESENT_INTERVAL_ONE;
+
+	if (d3d->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, window, D3DCREATE_HARDWARE_VERTEXPROCESSING, &presentParameters, &device) < 0)
+		return false;
+
+	return true;
 }
 
-void ResetDevice() noexcept
+void gui::ResetDevice() noexcept
 {
+	ImGui_ImplDX9_InvalidateDeviceObjects();
 
+	const auto result = device->Reset(&presentParameters);
+
+	if (result == D3DERR_INVALIDCALL)
+		IM_ASSERT(0);
+
+	ImGui_ImplDX9_CreateDeviceObjects();
 }
 
-void DestroyDevice() noexcept
+void gui::DestroyDevice() noexcept
 {
+	if (device)
+	{
+		device->Release();
+		device = nullptr;
+	}
 
+	if (d3d)
+	{
+		d3d->Release();
+		d3d = nullptr;
+	}
 }
 
-void CreateImGui() noexcept
+void gui::CreateImGui() noexcept
 {
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO io = ::ImGui::GetIO();
 
+	io.IniFilename = NULL;
+
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplWin32_Init(window);
+	ImGui_ImplDX9_Init(device);
 }
 
-void DestroyImGui() noexcept
+void gui::DestroyImGui() noexcept
 {
-
+	ImGui_ImplDX9_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
 }
 
-void BeginRender() noexcept
+void gui::BeginRender() noexcept
 {
+	MSG message;
+	while (PeekMessage(&message, 0, 0, 0, PM_REMOVE))
+	{
+		TranslateMessage(&message);
+		DispatchMessage(&message);
 
+		if (message.message == WM_QUIT)
+		{
+			isOpened = !isOpened;
+			return;
+		}
+	}
+
+	// Start of the ImGui frame
+	ImGui_ImplDX9_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
 }
 
-void Render() noexcept
+void gui::Render() noexcept
 {
+	ImGui::SetNextWindowPos({ 0, 0 });
+	ImGui::SetNextWindowSize({ WIDTH, HEIGHT });
+	ImGui::Begin("Celestial", &isOpened, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove );
 
+
+	ImGui::End();
 }
 
-void EndRender() noexcept
+void gui::EndRender() noexcept
 {
+	ImGui::EndFrame();
 
+	device->SetRenderState(D3DRS_ZENABLE, FALSE);
+	device->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
+	device->SetRenderState(D3DRS_SCISSORTESTENABLE, FALSE);
+
+	device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_RGBA(0, 0, 0, 255), 1.0f, 0);
+
+	if (device->BeginScene() >= 0)
+	{
+		ImGui::Render();
+		ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+		device->EndScene();
+	}
+
+	const auto result = device->Present(0, 0, 0, 0);
+
+	// handle loss of D3D9 device
+	if (result == D3DERR_DEVICELOST && device->TestCooperativeLevel() == D3DERR_DEVICENOTRESET)
+		ResetDevice();
 }
