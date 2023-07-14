1f82d1b9b635b189e5dbc928db7920be73915dd3
estkz <3stetik@gmail.com>
2023-07-14 03:09:49 +0200

    init

---
+++ Celestial/main.cpp
@@ -0,0 +1,32 @@
+/*
+	[+] ---------- Celestial ---------- [+]
+	
+		• Developed by: estetik (loab)
+		 • Build: Alpha
+		 • Date: 14-7-2023
+		• Educational Purposes Only
+
+	[+] ------------------------------- [+]
+*/
+
+
+// Gui Function Includes
+#include "menu/gui.h"
+
+// Miscellaneous Related Includes
+#include <thread>
+
+int __stdcall wWinMain(HINSTANCE instance, HINSTANCE previousInstance, PWSTR arguments, int commandShow)
+{
+	// TODO:
+	// Create The Graphical User Interface
+	// Create while isOpened loop
+	// Destroy imgui/Device/HWindow to avoid memory leaks
+
+
+
+
+	// Once EXIT_SUCCES gets returned it indicates that the cheat has executed successfully and is now terminating (shutting down).
+	return EXIT_SUCCESS;
+}
+
---
+++ Celestial/menu/gui.cpp
@@ -0,0 +1,18 @@
+// Menu Related Includes
+#include "../menu/gui.h"
+
+#include "../imgui/imgui_impl_dx9.h"
+#include "../imgui/imgui_impl_win32.h"
+#include "../imgui/imgui_internal.h"
+
+#include <d3dx9.h>
+#include <d3d9.h>
+#include <d3dx9tex.h>
+
+#pragma comment (lib, "d3d9.lib")
+#pragma comment (lib, "d3dx9.lib")
+
+// Miscellaneous Related Includes
+#include <vector>
+#include <string>
+
