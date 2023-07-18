0f0be7a0d723dafa6c02d5bf49e25f9554469c2e
estkz <3stetik@gmail.com>
2023-07-18 23:56:36 +0200

    init

---
+++ Celestial/memory/offsets.h
@@ -0,0 +1,38 @@
+#pragma once
+
+/*
+
+	TODO: Define all offsets in stead of commented out.
+
+	dsPlayer = ac_client.exe+17E0A8
+	dsEntity = "ac_client.exe"+18AC04
+
+	// Local Player
+	health = 0xEC
+	armor = 0xF0
+
+	// Hacks
+	rapidfire = 0x164
+
+	// Entities
+	distance between entities = 4 bytes 0x4
+
+	// Gun Ammo
+	MK = 0x12C
+	TPM = 0x130
+	V19 = 0x134
+	ARD = 0x138
+	AD = 0x13C
+	MK(r) = 0x148
+	MTP = 0x140
+	Nade = 0x144
+
+	// Viewangles
+	pViewangleWidth = 0x34
+	pViewangleHeight = 0x38
+
+	// Coordinates
+	coordX = 0x28
+	coordY = 0x2c
+	coordZ = 0x30
+*/
