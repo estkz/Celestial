5c24ae200aa16604cbab08df1f9c21f0561c044f
estkz <3stetik@gmail.com>
2023-08-31 02:07:44 +0200

    init

--- Celestial/features/Trainer/infAmmo.cpp
+++ Celestial/features/Trainer/infAmmo.cpp
@@ -8,7 +8,7 @@ void infiniteAmmo()
 {
     const auto moduleBase = memory.GetModuleAddress("ac_client.exe");
     const auto localPlayerPtr = memory.Read<std::uintptr_t>(moduleBase + localPlayer);
-    const auto ammoAddress = localPlayerPtr + 0x138; // Temp offset, will make this work on all weapons soon
+    const auto ammoAddress = localPlayerPtr + 0x140; // Temp offset, will make this work on all weapons soon
 
     const int newAmmo = 1337;
     memory.Write<int>(ammoAddress, newAmmo);
