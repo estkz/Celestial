4871814136fc4b6e9a1bd3c1e76479d47638f9f4
estkz <3stetik@gmail.com>
2023-09-05 08:45:18 +0200

    init

--- Celestial/features/Misc/bhop.cpp
+++ Celestial/features/Misc/bhop.cpp
@@ -4,13 +4,7 @@
 
 void bhop()
 {
-    Beep(330, 100); Sleep(100);
-    Beep(330, 100); Sleep(300);
-    Beep(330, 100); Sleep(300);
-    Beep(262, 100); Sleep(100);
-    Beep(330, 100); Sleep(300);
-    Beep(392, 100); Sleep(700);
-    Beep(196, 100); Sleep(700);
+   
 }
 
 
