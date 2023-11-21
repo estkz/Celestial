dee416c163b7baeef4334a7abd8e34a031559f42
syulen <3stetik@gmail.com>
2023-11-21 04:35:34 +0100

    init

--- Celestial/menu/gui.h
+++ Celestial/menu/gui.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <d3d9.h>
 
-// Test
 // We create this namespace with all the functions used for setting up the imgui window
 // The declared functions and variables down below can be accessed using the scope resolution operator '::'
 // Example of usage: gui::
